80302a7bdb9b54f8ed8cb060a8e5774f8c2d46dc
Fernando Giorgetti <fgiorgetti@users.noreply.github.com>
2023-07-24 10:44:28 -0300

    Release 1.4.2 (#31)

--- env.sh
+++ env.sh
@@ -1,23 +1,23 @@
 export INTERACTIVE=false
 
 # The new version being defined, i.e.: 1.4.0
-export NEW_VERSION="1.4.1"
+export NEW_VERSION="1.4.2"
 
 # The current version to be used as a source for the new one, i.e.: 1.4.0-rc2
-export CUR_VERSION="1.4.0-rc3"
+export CUR_VERSION="1.4.1"
 
 # The latest released version to be replaced (non rc), i.e.: 1.3.0
-export REPLACES_VERSION="1.3.0"
+export REPLACES_VERSION="1.4.1"
 
 # The tag to be used for the skupper-router image
-export SKUPPER_ROUTER_TAG="2.4.1"
+export SKUPPER_ROUTER_TAG="2.4.2"
 
 # The tag to be used for the control plane images:
 # site-controller, service-controller, config-sync, flow-collector
-export SKUPPER_CONTROL_TAG="1.4.1"
+export SKUPPER_CONTROL_TAG="1.4.2"
 
 # Comma separated list of versions to be skipped (optional)
 # example:
 # SKIP_VERSIONS="1.4.0-rc2,1.4.0-rc3"
 #unset SKIP_VERSIONS
-export SKIP_VERSIONS="1.4.0-rc2,1.4.0-rc3"
+#export SKIP_VERSIONS="1.4.0-rc2,1.4.0-rc3"
