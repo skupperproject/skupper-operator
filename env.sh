1efd4f334e1937649826f70982faa61c7037edbf
Fernando Giorgetti <fgiorget@redhat.com>
2025-05-08 15:46:53 -0300

    Release 1.9.1

--- env.sh
+++ env.sh
@@ -1,20 +1,20 @@
 export INTERACTIVE=false
 
 # The new version being defined, i.e.: 1.4.0
-export NEW_VERSION="1.9.0"
+export NEW_VERSION="1.9.1"
 
 # The current version to be used as a source for the new one, i.e.: 1.4.0-rc2
-export CUR_VERSION="1.8.4"
+export CUR_VERSION="1.9.0"
 
 # The latest released version to be replaced (non rc), i.e.: 1.3.0
-export REPLACES_VERSION="1.8.4"
+export REPLACES_VERSION="1.9.0"
 
 # The tag to be used for the skupper-router image
 export SKUPPER_ROUTER_TAG="2.7.5"
 
 # The tag to be used for the control plane images:
 # site-controller, service-controller, config-sync, flow-collector
-export SKUPPER_CONTROL_TAG="1.9.0"
+export SKUPPER_CONTROL_TAG="1.9.1"
 
 # The tag to be used for the prometheus image
 export PROMETHEUS_TAG="v2.55.1"
